254fd2220b66c8a2969040bbc18f156376b3e693
Nazar-Pa <nezer.pashayev@gmail.com>
2020-02-06 12:34:03 +0100

    initial setup

---
+++ server.js
@@ -0,0 +1,39 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+//const bodyParser = require('body-parser')
+const indexRouter = require('./routes/index')
+//const authorRouter = require('./routes/authors')
+//const bookRouter = require('./routes/books')
+
+
+//const methodOverride = require('method-override')
+
+
+// we gonna use ejs as our view-engine
+app.set('view engine', 'ejs')
+//where are views are coming from, current folder + /views 
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+//app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
+//app.use(methodOverride('_method'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+
+app.use('/', indexRouter)
+//app.use('/authors', authorRouter)
+//app.use('/books', bookRouter)
+
+app.listen(process.env.PORT || 4000)
